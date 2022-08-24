1c2f24ff6d32c14ee5a1a751226d60aff5eef083
nagy nabil <nagynabil651@gmail.com>
2022-08-24 04:19:43 +0200

    hero

--- dist/resources/user/user.router.js
+++ dist/resources/user/user.router.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
-import { me, updateMe } from "./user.controller.js";
+import { updateMe } from "./user.controller.js";
 const userRouter = Router();
 userRouter.route("/me")
-    .get(me)
-    .post(updateMe);
-// .get((req, res) => {
-//     res.status(200).send(`
-//       <h2>With <code>"express"</code> npm package</h2>
-//       <form action="/api/user/me" enctype="multipart/form-data" method="post">
-//         <div>Text field title: <input type="text" name="username" /></div>
-//         <div>Text field title: <input type="text" name="email" /></div>
-//         <div>Text field title: <input type="text" name="first_name" /></div>
-//         <div>File: <input type="file" name="avatar" multiple="multiple"  /></div>
-//         <input type="submit" value="Upload" />
-//       </form>
-//     `);
-//   })
+    // .get(me)
+    .post(updateMe)
+    .get((req, res) => {
+    res.status(200).send(`
+      <h2>With <code>"express"</code> npm package</h2>
+      <form action="/api/user/me" enctype="multipart/form-data" method="post">
+        <div>Text field title: <input type="text" name="username" /></div>
+        <div>Text field title: <input type="text" name="email" /></div>
+        <div>Text field title: <input type="text" name="first_name" /></div>
+        <div>File: <input type="file" name="avatar" multiple="multiple"  /></div>
+        <input type="submit" value="Upload" />
+      </form>
+    `);
+});
 export default userRouter;
