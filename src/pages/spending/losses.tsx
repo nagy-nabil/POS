25909f0ef0a91b56dd49db2fdee114b03db0aa5e
nagy nabil <nagynabil651@gmail.com>
2023-08-05 17:09:27 +0300

    init spendings/losses tables

--- src/pages/spending/losses.tsx
+++ src/pages/spending/losses.tsx
@@ -1,19 +1,38 @@
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React, { useMemo, type ReactElement } from "react";
 import type { GetStaticPropsContext } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { type NextPageWithLayout } from "../_app";
-import { type ReactElement } from "react";
-import Layout from "@/components/layout";
-import { useAuth } from "@/hooks/useAuth";
 import Head from "next/head";
-import { useTranslation } from "react-i18next";
+import IndeterminateCheckbox from "@/components/form/indeterminateCheckbox";
+import Layout from "@/components/layout";
 import LossesModal from "@/components/modal/lossesModal";
+import TableBody from "@/components/table/body";
+import { fuzzyFilter } from "@/components/table/helpers";
+import TableUtils from "@/components/table/utils";
+import { useAuth } from "@/hooks/useAuth";
+import { api } from "@/utils/api";
+import { dateFormater } from "@/utils/date";
+import { type Loss } from "@prisma/client";
+import { type RankingInfo } from "@tanstack/match-sorter-utils";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  getFacetedMinMaxValues,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+  type ColumnFiltersState,
+  type FilterFn,
+  type SortingState,
+} from "@tanstack/react-table";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "react-i18next";
+
+import { type NextPageWithLayout } from "../_app";
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
-  console.log(
-    "🪵 [spending.tsx:15] ~ token ~ \x1b[0;32mlocale\x1b[0m = ",
-    locale
-  );
   return {
     props: {
       // only pass array of required namespace to the page to make use of translitions code spliting
@@ -26,9 +45,153 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
   };
 }
 
+declare module "@tanstack/table-core" {
+  interface FilterFns {
+    fuzzy: FilterFn<unknown>;
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo;
+  }
+}
+
+const columnHelper = createColumnHelper<Loss>();
+
+function Table(props: { data: Loss[] }) {
+  const { t } = useTranslation();
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [rowSelection, setRowSelection] = React.useState({});
+  const columns = useMemo(
+    () => [
+      columnHelper.display({
+        id: "select",
+        header: ({ table }) => (
+          <div className="relative m-auto">
+            <IndeterminateCheckbox
+              {...{
+                checked: table.getIsAllRowsSelected(),
+                indeterminate: table.getIsSomeRowsSelected(),
+                onChange: table.getToggleAllRowsSelectedHandler(),
+              }}
+            />
+          </div>
+        ),
+        cell: ({ row }) => (
+          <div className="px-1">
+            <IndeterminateCheckbox
+              {...{
+                checked: row.getIsSelected(),
+                disabled: !row.getCanSelect(),
+                indeterminate: row.getIsSomeSelected(),
+                onChange: row.getToggleSelectedHandler(),
+              }}
+            />
+          </div>
+        ),
+      }),
+      columnHelper.accessor("id", {
+        header: () => <span>{t("table.common.id")}</span>,
+        cell: (info) => info.getValue(),
+        enableSorting: false,
+      }),
+      columnHelper.accessor("name", {
+        header: () => <span>{t("table.common.name")}</span>,
+        cell: (info) => info.getValue(),
+        filterFn: "fuzzy",
+      }),
+      columnHelper.accessor("description", {
+        header: () => <span>{t("table.loss.description")}</span>,
+        cell: (info) => info.getValue(),
+      }),
+      columnHelper.accessor("additionalAmount", {
+        header: () => <span>{t("table.loss.additionalAmount")}</span>,
+        cell: (info) => info.getValue(),
+      }),
+      columnHelper.accessor("createdAt", {
+        header: () => <span>{t("table.common.createdAt")}</span>,
+        cell: (info) => dateFormater.format(info.getValue()),
+        enableColumnFilter: false,
+      }),
+    ],
+    [t]
+  );
+
+  const table = useReactTable({
+    data: props.data,
+    columnResizeMode: "onChange",
+    getCoreRowModel: getCoreRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+      rowSelection,
+    },
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    columns,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getFacetedMinMaxValues: getFacetedMinMaxValues(),
+    getPaginationRowModel: getPaginationRowModel(),
+
+    enableRowSelection: true,
+    onRowSelectionChange: setRowSelection,
+    defaultColumn: {
+      minSize: 200,
+    },
+  });
+
+  if (typeof window === "undefined") return null;
+
+  return (
+    <div className="mt-3 flex flex-col">
+      <div className="w-screen">
+        {/* item utils*/}
+        <div className="flex justify-start gap-3">
+          {/* <ProductModal defaultValues={{}} operationType="post" />
+          {Object.keys(rowSelection).length === 1 ? (
+            <ProductModal
+              key={"updateProduct"}
+              operationType="put"
+              // @ts-ignore
+              defaultValues={props.data[+Object.keys(rowSelection)[0]]}
+            />
+          ) : null} */}
+        </div>
+
+        <div className="w-full overflow-x-auto">
+          <TableBody table={table} />
+        </div>
+
+        {/* table utils */}
+        <TableUtils table={table} />
+      </div>
+    </div>
+  );
+}
+
 const Spending: NextPageWithLayout = () => {
   const { t } = useTranslation();
-  const {} = useAuth({ noExistRedirectTo: "/signin" });
+  const { token, setToken } = useAuth({ noExistRedirectTo: "/signin" });
+  const lossQuery = api.losses.getMany.useQuery(undefined, {
+    staleTime: Infinity,
+    enabled: !!token,
+    retry(_failureCount, error) {
+      if (error.data?.code === "UNAUTHORIZED") {
+        setToken("").catch((e) => {
+          throw e;
+        });
+        return false;
+      }
+      return true;
+    },
+  });
 
   return (
     <>
@@ -40,6 +203,7 @@ const Spending: NextPageWithLayout = () => {
           <h1 className="text-4xl">{t("pages.spending.header")}</h1>
         </header>
         <LossesModal defaultValues={{}} operationType="post" />
+        {lossQuery.data && <Table data={lossQuery.data} />}
       </div>
       <ReactQueryDevtools initialIsOpen={false} />
     </>
