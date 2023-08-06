8fbc8b24eb2229e369074fe8355e6954e987b0d3
nagy nabil <nagynabil651@gmail.com>
2023-08-07 01:43:36 +0300

    migrate to prisma 5

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,29 @@
-/** @type {import("prettier").Config} */
+/** @type {import("prettier").Config & import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  plugins: [
+    require.resolve("prettier-plugin-tailwindcss"),
+    require.resolve("@ianvs/prettier-plugin-sort-imports"),
+  ],
+  endOfLine: "lf",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "^(expo(.*)$)|^(expo$)",
+    "<THIRD_PARTY_MODULES>",
+    "^@acme/(.*)$",
+    "",
+    "^~/utils/(.*)$",
+    "^~/components/(.*)$",
+    "^~/styles/(.*)$",
+    "^~/(.*)$",
+    "^[./]",
+  ],
+  // importOrderSeparation: false,
+  // importOrderSortSpecifiers: true,
+  // importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  // importOrderMergeDuplicateImports: true,
+  // importOrderCombineTypeAndValueImports: true,
   overrides: [
     {
       files: "*.md",
