bbb1c68d8568a8959d24428a97f9503472228e42
nagy nabil <nagynabil651@gmail.com>
2023-12-01 01:59:42 +0200

    fix: revenue should be `buyPrice * stock`

--- src/server/api/routers/dashboard.ts
+++ src/server/api/routers/dashboard.ts
@@ -1,43 +1,47 @@
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "@/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import z from "zod";
+
 // import { TRPCError } from "@trpc/server";
 
 // here suppose to add logic to add new org members and so on
 export const dashboardRouter = createTRPCRouter({
   revenue: protectedProcedure.query(async ({ ctx }) => {
     // i'm not in the mood to fuck with prisma
-    const rev = await ctx.prisma.$queryRaw<[{ revenue: number }]>`select sum("sellPrice" * stock) as revenue from "Product";`;
+    const rev = await ctx.prisma.$queryRaw<
+      [{ revenue: number }]
+    >`select sum("buyPrice" * stock) as revenue from "Product";`;
     return rev[0];
   }),
   /**
    * return one product selling history
    * @param productId
    */
-  productHistory: protectedProcedure.input(z.string()).query(async ({input, ctx }) => {
-    return await ctx.prisma.product.findFirst({
-      where: {
-      id: input,
-      },
-      include: {
-        orders: {
-          orderBy:[{
-            order: {
-              createdAt: 'desc'
-            }
-          }],
-          include: {
-            order: {
-              select: {
-                id: true,
-                createdAt: true,
-              }
-            }
-          }
-        }
-      }
-    })
-  }),
+  productHistory: protectedProcedure
+    .input(z.string())
+    .query(async ({ input, ctx }) => {
+      return await ctx.prisma.product.findFirst({
+        where: {
+          id: input,
+        },
+        include: {
+          orders: {
+            orderBy: [
+              {
+                order: {
+                  createdAt: "desc",
+                },
+              },
+            ],
+            include: {
+              order: {
+                select: {
+                  id: true,
+                  createdAt: true,
+                },
+              },
+            },
+          },
+        },
+      });
+    }),
 });
